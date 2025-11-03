cd3bf8548fe5b2a336b0477dc451faefbd8fedef
Juan Pablo Sacco Rey <89319@sistemas.frc.utn.edu.ar>
2025-11-03 16:29:46 -0300

    cambiamos archivos properties a yml y agregamos test para comprobar puertos

---
+++ operaciones-service/src/main/java/tpi/transporte/operaciones/controller/TestController.java
@@ -0,0 +1,16 @@
+package tpi.transporte.operaciones.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/") // El gateway ya quitó el prefijo /operaciones
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        // Este mensaje nos confirmará que la petición llegó a ESTE servicio
+        return "Respuesta desde Operaciones Service!";
+    }
+}
